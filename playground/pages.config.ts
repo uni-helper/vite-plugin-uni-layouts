25173cd5b0b3d7c292fa8ff7b8f8e306fd957f8e
KeJunMao <kejun1997@gmail.com>
2023-12-11 13:41:45 +0800

    chore: add pages plugin

---
+++ playground/pages.config.ts
@@ -0,0 +1,10 @@
+import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
+
+export default defineUniPages({
+  globalStyle: {
+    navigationBarTextStyle: 'black',
+    navigationBarTitleText: 'uni-app',
+    navigationBarBackgroundColor: '#F8F8F8',
+    backgroundColor: '#F8F8F8',
+  },
+})
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [UniLayouts(), uni()],
+  plugins: [
+    // @ts-expect-error ignore
+    UniPages({
+      subPackages: [
+        'pages-sub',
+      ],
+      dts: 'src/uni-pages.d.ts',
+    }),
+    // @ts-expect-error ignore
+    UniLayouts(),
+    uni(),
+  ],
 })
